1567396a7095f6636750e83880d7eccc2ef4ddb2
oIgorFilipi <134612614+oigorfilipi@users.noreply.github.com>
2026-03-24 19:54:47 -0300

    Inicio do Projeto: Criação da Navbar - P1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  faBars,
+  faSearch,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="Nav">
+          <div className="Inner-Content">
+            <h1 className="Logo">
+              BOOK<span>STORE</span>
+            </h1>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/products">Produtos</Link>
+                </li>
+                <li>
+                  <Link to="/about">Sobre</Link>
+                </li>
+                <li>
+                  <Link to="/contacts">Contatos</Link>
+                </li>
+                <li>
+                  <Link to="/account">Conta</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="Navs-Icon-Container">
+              <div className="Search-Input-Container">
+                <input type="search" placeholder="Procurar"></input>
+                <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+              </div>
+              <button className="Shopping-Cart">
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+                <div className="Products-Count">12</div>
+                <button className="Menu-Button">
+                  <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+                </button>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import {
+  faBars,
+  faSearch,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className="Nav">
+      <div className="Inner-Content">
+        <h1 className="Logo">
+          BOOK<span>STORE</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Produtos</Link>
+            </li>
+            <li>
+              <Link to="/about">Sobre</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contatos</Link>
+            </li>
+            <li>
+              <Link to="/account">Conta</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="Navs-Icon-Container">
+          <div className="Search-Input-Container">
+            <input type="search" placeholder="Procurar"></input>
+            <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+          </div>
+          <button className="Shopping-Cart">
+            <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            <div className="Products-Count">12</div>
+            <button className="Menu-Button">
+              <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+            </button>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
